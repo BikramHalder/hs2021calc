e9964b201005cf4d13bb2ca2ed283020f7844871
BikramHalder <74728707+BikramHalder@users.noreply.github.com>
2021-06-20 22:32:36 +0530

    Major changes in code
    Added ,
    1. Madhyamik Total (Marks of best 4) & Percentage
    2. checking for all marks are valid or not
    3. Another modal "Oops, Put your marks properly" if returned false in (2)
    
    and some minor correction

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // subjects : Bengali, English, Hindi, Nepali, Urdu, Santhali, Odia, Telegu, Gujrati, Punjabi, Physics, Chemistry, Mathematics, Biology, Computer Science, Computer Applicaton, Philosophy, Education, Geography, Statistics, Political Science, Psychology, Agronomy, Anthropology, Accountancy, Busneiss Studies, Economics, History, Enviornmental Studies, Nutrition, Home Management, Journalism, Physical Education, Commercial Law & Preliminaries of Auditing, Costing & Taxation, Sanskrit, Persian, Arabic, French
 
 
-const nonLab = [ "BNGA", "ENGA", "HINA", "SANA", "ODIA", "TELA", "URDA", "GUJA", "PUNA", "NEPA", "ENGB", "BNGB", "HINB", "NEPB", "MATH", "HIST","ECO", "PHYC", "PHIL", "EDU", "POLS", "JOUR", "CATX", "CLPA", "ACCN", "BUST", "SANS", "ARAB", "FREN", "PERS", "AGRO", "ATHR", "EVS", "HMG" ];
+const nonLab = [ "BNGA", "ENGA", "HINA", "SANA", "ODIA", "TELA", "URDA", "GUJA", "PUNA", "NEPA", "ENGB", "BNGB", "HINB", "NEPB", "MATH", "HIST", "ECO", "PHYC", "PHIL", "EDU", "POLS", "JOUR", "CATX", "CLPA", "ACCN", "BUST", "SANS", "ARAB", "FREN", "PERS", "AGRO", "ATHR", "EVS", "HMG" ];
 const Lab = [ "PHYS", "CHEM", "BIOS", "COMS", "COMA", "STAT", "GEOG", "NUTN" ];
 
 let form2 = document.getElementById( 'form2' );
@@ -39,6 +39,7 @@ function placeholder()
     }
 }
 
+
 // selecting the calculate button
 let calBtn = document.getElementById( 'calBtn' );
 calBtn.addEventListener( 'click', () =>
@@ -48,6 +49,7 @@ calBtn.addEventListener( 'click', () =>
         SEM.push( Number( document.getElementById( `Sub${ i + 1 }` ).value ) );
     }
     let SEFinal = SEM.reduce( ( a, b ) => a + b, 0 );
+
     let marks = [], sub = []; // XI & XII marks array
     for ( let i = 0; i < 6; i++ ) {
         marks.push( {
@@ -58,21 +60,31 @@ calBtn.addEventListener( 'click', () =>
         } );
         sub.push( marks[ i ].sub );
     }
+    let validMarks = Array.from( marks, e => e.theory * e.prc ).reduce( ( a, b ) => a * b, 1 );
 
-    
-    // Bootstrap Modal Dialogue
-    let myModal = new bootstrap.Modal( document.getElementById( 'myModal' ), {
+    // Bootstrap Modal Dialogue 1
+    let myModal_1 = new bootstrap.Modal( document.getElementById( 'myModal_1' ), {
         keyboard: false
     } );
-    document.getElementById( 'hideModal' ).addEventListener( 'click', () => { myModal.hide(); console.clear(); } );
+    document.getElementById( 'hideModal_1' ).addEventListener( 'click', () => { myModal_1.hide(); console.clear(); } );
+
+    // Bootstrap Modal Dialogue 1
+    let myModal_2 = new bootstrap.Modal( document.getElementById( 'myModal_2' ), {
+        keyboard: false
+    } );
+    document.getElementById( 'hideModal_2' ).addEventListener( 'click', () => { myModal_2.hide(); console.clear(); } );
+
 
     // Check if any of 2 elective subjects are equal
     if ( sub[ 2 ] == sub[ 3 ] || sub[ 2 ] == sub[ 4 ] || sub[ 2 ] == sub[ 5 ] || sub[ 3 ] == sub[ 4 ] || sub[ 3 ] == sub[ 5 ] || sub[ 4 ] == sub[ 5 ] ) {
-        myModal.show();
+        myModal_1.show();
     }
-    else {
+    else if ( validMarks == NaN || validMarks == 0 ) {
+        myModal_2.show();
+    }
+    else {  // inject the html code
 
-        let html = `<table class="table table-striped">
+        let html_2 = `<table class="table table-striped">
         <thead>
             <tr>
                 <th scope="col">Subjects</th>
@@ -86,13 +98,13 @@ calBtn.addEventListener( 'click', () =>
         {
             e.total = Number( Math.round( ( SEFinal / 400 ) * 0.4 * ( 100 - isLab( e.sub ) ) + ( 0.6 * e.theory ) + e.prc ) );
             total.push( e.total );
-            html += `<tr>
+            html_2 += `<tr>
                 <th scope="row">${ e.sub }</th>
                 <td>${ e.total }</td>
             </tr>`;
         } );
 
-        html += `
+        html_2 += `
                 <tr class="table-info">
                     <th>Total Marks(out of 500 - for best of 5)</th>
                     <td>${ total.reduce( ( a, b ) => a + b, 0 ) - Math.min( ...total ) }</td>
@@ -104,13 +116,27 @@ calBtn.addEventListener( 'click', () =>
             </tbody>
         </table>`;
 
-        let markSheet = document.getElementById( 'markSheet' );
-        markSheet.innerHTML = html;
+        document.getElementById( 'markSheet' ).innerHTML = html_2;
+
+        document.getElementById( 'SEMarks' ).innerHTML = `
+    <table class="table table-striped">
+        <tbody>
+            <tr class="table-info">
+                <th>Madhyamik Total (Marks of best 4)</th>
+                <td>${ SEFinal }</td>
+            </tr>
+            <tr class="table-success">
+                <th>Percentage</th>
+                <td>${ SEFinal / 4 }%</td>
+            </tr>
+        </tbody>
+    </table>`;
+
     }
     console.clear();
 } );
 
-// checking eheather the suject is lob or non lab
+// checking wheather the suject is lob or non lab
 function isLab( sub ) 
 {
     if ( Lab.find( e => e == sub ) == sub ) {
