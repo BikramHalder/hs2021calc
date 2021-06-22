6b894713b24cb962c737b90bb3ff5a9fbd4010a0
BikramHalder <74728707+BikramHalder@users.noreply.github.com>
2021-06-22 21:09:38 +0530

    Added more checking and proper subject names

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // subjects : Bengali, English, Hindi, Nepali, Urdu, Santhali, Odia, Telegu, Gujrati, Punjabi, Physics, Chemistry, Mathematics, Biology, Computer Science, Computer Applicaton, Philosophy, Education, Geography, Statistics, Political Science, Psychology, Agronomy, Anthropology, Accountancy, Busneiss Studies, Economics, History, Enviornmental Studies, Nutrition, Home Management, Journalism, Physical Education, Commercial Law & Preliminaries of Auditing, Costing & Taxation, Sanskrit, Persian, Arabic, French
 
 
-const nonLab = [ "BNGA", "ENGA", "HINA", "SANA", "ODIA", "TELA", "URDA", "GUJA", "PUNA", "NEPA", "ENGB", "BNGB", "HINB", "NEPB", "MATH", "HIST", "ECO", "PHYC", "PHIL", "EDU", "POLS", "JOUR", "CATX", "CLPA", "ACCN", "BUST", "SANS", "ARAB", "FREN", "PERS", "AGRO", "ATHR", "EVS", "HMG" ];
-const Lab = [ "PHYS", "CHEM", "BIOS", "COMS", "COMA", "STAT", "GEOG", "NUTN" ];
+const nonLab = [ "Bengali", "English", "Hindi", "Snathali", "Odia", "Telegu", "Urdu", "Gujrati", "Punjabi", "Nepali", "Mathematics", "History", "Economics", "Psychology", "Philosophy", "Education", "Political Science", "Journalism", "Costing & Taxation", "Commercial Law & Preliminaries of Auditing", "Accountancy", "Busneiss Studies", "Sanskrit", "Arabic", "French", "Persian", "Agronomy", "Anthropology", "Enviornmental Studies", "Home Management" ];
+const Lab = [ "Physics", "Chemistry", "Biology", "Computer Science", "Modern Computer Applicaton", "Statistics", "Geography", "Nutrition" ];
 
 let form2 = document.getElementById( 'form2' );
 let form1 = document.getElementById( 'form1' );
@@ -12,7 +12,7 @@ form2.addEventListener( 'touchstart', placeholder );
 form1.addEventListener( 'touchstart', placeholder );
 form2.addEventListener( 'load', placeholder );
 
-// writing proper plceholder in proper place
+// Writing proper plceholder in proper place
 function placeholder() 
 {
     for ( let i = 0; i < 4; i++ ) {
@@ -44,13 +44,14 @@ function placeholder()
 let calBtn = document.getElementById( 'calBtn' );
 calBtn.addEventListener( 'click', () =>
 {
-    let SEM = [];   // Madhyamik marks array
+    let SEM = [], SEFinal = 0;   // Madhyamik marks array, total
     for ( let i = 0; i < 4; i++ ) {
-        SEM.push( Number( document.getElementById( `Sub${ i + 1 }` ).value ) );
+        let n = Number( document.getElementById( `Sub${ i + 1 }` ).value );
+        SEM.push( n );
+        SEFinal += n;
     }
-    let SEFinal = SEM.reduce( ( a, b ) => a + b, 0 );
-
-    let marks = [], sub = []; // XI & XII marks array
+    
+    let marks = [], sub = []; // XI & XII marks, Subject combination arrays
     for ( let i = 0; i < 6; i++ ) {
         marks.push( {
             "sub": document.getElementById( `HS${ i + 1 }` ).children[ 1 ].value,
@@ -60,31 +61,27 @@ calBtn.addEventListener( 'click', () =>
         } );
         sub.push( marks[ i ].sub );
     }
-    let validMarks = Array.from( marks, e => e.theory * e.prc ).reduce( ( a, b ) => a * b, 1 );
+    let validMarks = Array.from( marks, e => e.theory * e.prc ).reduce( ( a, b ) => a * b, 1 ) * SEM.reduce( ( a, b ) => a * b, 1 );
 
     // Bootstrap Modal Dialogue 1
-    let myModal_1 = new bootstrap.Modal( document.getElementById( 'myModal_1' ), {
-        keyboard: false
-    } );
+    let myModal_1 = new bootstrap.Modal( document.getElementById( 'myModal_1' ), { keyboard: false } );
     document.getElementById( 'hideModal_1' ).addEventListener( 'click', () => { myModal_1.hide(); console.clear(); } );
-
-    // Bootstrap Modal Dialogue 1
-    let myModal_2 = new bootstrap.Modal( document.getElementById( 'myModal_2' ), {
-        keyboard: false
-    } );
-    document.getElementById( 'hideModal_2' ).addEventListener( 'click', () => { myModal_2.hide(); console.clear(); } );
 
 
     // Check if any of 2 elective subjects are equal
     if ( sub[ 2 ] == sub[ 3 ] || sub[ 2 ] == sub[ 4 ] || sub[ 2 ] == sub[ 5 ] || sub[ 3 ] == sub[ 4 ] || sub[ 3 ] == sub[ 5 ] || sub[ 4 ] == sub[ 5 ] ) {
+        document.getElementById( 'modalBody' ).innerHTML = `Any 2 of elective subjects can't be same. Please choose your subjects properly.`;
         myModal_1.show();
     }
-    else if ( validMarks == NaN || validMarks == 0 ) {
-        myModal_2.show();
+    // Check if all the marks entered are valid
+    else if ( !validMarks ) {
+        document.getElementById( 'modalBody' ).innerHTML = `Put your marks properly.`;
+        myModal_1.show();
     }
     else {  // inject the html code
 
-        let html_2 = `<table class="table table-striped">
+        let html = `
+        <table class="table table-striped">
         <thead>
             <tr>
                 <th scope="col">Subjects</th>
@@ -93,18 +90,23 @@ calBtn.addEventListener( 'click', () =>
         </thead>
         <tbody>`;
 
-        let total = [];
+        let total = [], subChoose = 1;
         marks.forEach( e =>
         {
-            e.total = Number( Math.round( ( SEFinal / 400 ) * 0.4 * ( 100 - isLab( e.sub ) ) + ( 0.6 * e.theory ) + e.prc ) );
-            total.push( e.total );
-            html_2 += `<tr>
+            if ( isLab( e.sub ) == NaN ) { subChoose *= 0; }
+            else {
+                e.total = Number( Math.round( ( SEFinal / 400 ) * 0.4 * ( 100 - isLab( e.sub ) ) + ( 0.6 * e.theory ) + e.prc ) );
+                total.push( e.total );
+                html += `
+            <tr>
                 <th scope="row">${ e.sub }</th>
                 <td>${ e.total }</td>
             </tr>`;
+
+            }
         } );
 
-        html_2 += `
+        html += `
                 <tr class="table-info">
                     <th>Total Marks(out of 500 - for best of 5)</th>
                     <td>${ total.reduce( ( a, b ) => a + b, 0 ) - Math.min( ...total ) }</td>
@@ -116,7 +118,8 @@ calBtn.addEventListener( 'click', () =>
             </tbody>
         </table>`;
 
-        document.getElementById( 'markSheet' ).innerHTML = html_2;
+        document.getElementById( 'modalBody' ).innerHTML = `Choose subjects properly.`;
+        !subChoose ? myModal_1.show() : document.getElementById( 'markSheet' ).innerHTML = html;
 
         document.getElementById( 'SEMarks' ).innerHTML = `
     <table class="table table-striped">
@@ -136,7 +139,7 @@ calBtn.addEventListener( 'click', () =>
     console.clear();
 } );
 
-// checking wheather the suject is lob or non lab
+// Checking wheather the suject is lob or non lab
 function isLab( sub ) 
 {
     if ( Lab.find( e => e == sub ) == sub ) {
@@ -146,7 +149,6 @@ function isLab( sub )
         return 20;
     }
     else {
-//         console.error( 'Select subjects properly' );
-        return 0;
+        return NaN;
     }
 };
